c92f1f99dbcacce6c2f98d230876d03e76b058c1
Glori <128950766+GlorianaZelaya@users.noreply.github.com>
2023-06-15 03:47:53 +0000

    commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,28 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  //let laexcusa = (document.getElementById("excusa").innerHTML = generarExcusa());
+  document.getElementById("excusa").innerText = generarExcusa();
+};
+
+let generarExcusa = function() {
+  let who = [
+    "Una abuela",
+    "Mi perro",
+    "El vecino",
+    "Un Alien",
+    "El chofer del bus"
+  ];
+  let action = ["se comió", "robó", "mordió", "rompió", "insultó"];
+  let what = ["mi proyecto", "mi tarea", "mi maqueta", "el arroz con leche"];
+  let when = ["ayer", "mañana", "en la madrugada", "anoche", "hace 2 días"];
+
+  let quien = [Math.floor(Math.random() * who.length)];
+  let accion = [Math.floor(Math.random() * action.length)];
+  let que = [Math.floor(Math.random() * what.length)];
+  let cuando = Math.floor(Math.random() * when.length);
+
+  return (
+    who[quien] + " " + action[accion] + " " + what[que] + " " + when[cuando]
+  );
 };
