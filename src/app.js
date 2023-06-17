c5ec02b907cfb83848a78ae82d7b6af6ac1c18cb
Glori <128950766+GlorianaZelaya@users.noreply.github.com>
2023-06-17 02:04:03 +0000

    cambio errores

--- src/app.js
+++ src/app.js
@@ -7,18 +7,12 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  //let laexcusa = (document.getElementById("excusa").innerHTML = generarExcusa());
+  //Quité el comment extra que no tenía ninguna funcionalidad.
   document.getElementById("excusa").innerText = generarExcusa();
 };
 
 let generarExcusa = function() {
-  let who = [
-    "Una abuela",
-    "Mi perro",
-    "El vecino",
-    "Un Alien",
-    "El chofer del bus"
-  ];
+  let who = ["Una abuela","Mi perro","El vecino","Un Alien", "El chofer del bus"];
   let action = ["se comió", "robó", "mordió", "rompió", "insultó"];
   let what = ["mi proyecto", "mi tarea", "mi maqueta", "el arroz con leche"];
   let when = ["ayer", "mañana", "en la madrugada", "anoche", "hace 2 días"];
@@ -26,7 +20,7 @@ let generarExcusa = function() {
   let quien = [Math.floor(Math.random() * who.length)];
   let accion = [Math.floor(Math.random() * action.length)];
   let que = [Math.floor(Math.random() * what.length)];
-  let cuando = Math.floor(Math.random() * when.length);
+  let cuando = [Math.floor(Math.random() * when.length)]; //Le puse unos corchetes cuadrados que le hacian falta, y se vea igual que los demas.
 
   return (
     who[quien] + " " + action[accion] + " " + what[que] + " " + when[cuando]
